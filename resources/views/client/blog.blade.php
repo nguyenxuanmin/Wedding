092a77cbe14cb2aa11c4c129c9f0d39c12f729c2
nguyenxuanmin <nguyenxuanmin1998@gmail.com>
2026-04-09 11:36:32 +0700

    amend web

--- resources/views/client/blog.blade.php
+++ resources/views/client/blog.blade.php
@@ -13,15 +13,25 @@
                     @foreach ($blogs as $blog)
                         <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-3 mb-lg-4">
                             <div class="item-index">
-                                <a href="{{route('blog_detail',['slug' => $blog->slug])}}">
                                     <div class="item-index-image">
-                                        <div class="icon-hover"><i class="fa fa-mail-forward"></i></div>
+                                        <a href="{{route('blog_detail',['slug' => $blog->slug])}}">
+                                            <div class="icon-hover"><i class="fa fa-mail-forward"></i></div>
+                                        </a>
                                         <img src="{{asset('storage/blogs/' . basename($blog->image))}}" alt="{{data_get($blog,'name_'.$lang)}}" class="object-fit-cover w-100">
                                     </div>
-                                    <div class="item-index-title">
-                                        {{data_get($blog,'name_'.$lang)}}
+                                    <div class="item-index-content">
+                                        <span class="tag-blog">Blog</span>
+                                        <p class="title-blog">
+                                            <a href="{{route('blog_detail',['slug' => $blog->slug])}}">{{data_get($blog,'name_'.$lang)}}</a>
+                                        </p>
+                                        <hr class="title_break">
+                                        @if (!empty(data_get($blog,'description_'.$lang)))
+                                            <div class="desc-blog">
+                                                {{data_get($blog,'description_'.$lang)}}
+                                            </div>
+                                        @endif
                                     </div>
-                                </a>
+                                
                             </div>
                         </div>
                     @endforeach
@@ -31,3 +41,4 @@
         </section>
     @endif
 @endsection
+
